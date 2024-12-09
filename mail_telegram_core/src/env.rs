0fe6b821c8ce1e8f4af9b9f332197ad56b2afcfb
uinstinct <61635505+uinstinct@users.noreply.github.com>
2024-12-09 19:23:43 +0530

    added token.json to .env

--- mail_telegram_core/src/env.rs
+++ mail_telegram_core/src/env.rs
@@ -2,9 +2,20 @@
 pub struct EnvVars {}
 
 impl EnvVars {
-    pub fn load_all_variables() -> Result<(), Box<dyn std::error::Error>> {
-        dotenvy::dotenv()?;
+    fn check_all_variables() -> Result<(), Box<dyn std::error::Error>> {
         std::env::var("DATABASE_URL")?;
+        std::env::var("TELEGRAM_BOT_TOKEN")?;
+        std::env::var("TELEGRAM_MY_CHAT_ID")?;
+        std::env::var("GMAIL_TOKEN_JSON")?;
+        Ok(())
+    }
+    
+    pub fn load_all_variables() -> Result<(), Box<dyn std::error::Error>> {
+        EnvVars::check_all_variables().unwrap_or_else(|_| {
+            dotenvy::dotenv().ok();
+        });
+        EnvVars::check_all_variables()?;
+        EnvVars::write_token_json_into_file();
         Ok(())
     }
     
@@ -18,5 +29,14 @@ impl EnvVars {
 
     pub fn telegram_my_chat_id() -> i64 {
         std::env::var("TELEGRAM_MY_CHAT_ID").unwrap().parse::<i64>().unwrap()
+    }
+
+    fn write_token_json_into_file() {
+        std::fs::write("./token.json", std::env::var("GMAIL_TOKEN_JSON").unwrap_or_else(|_| {
+            println!("GMAIL_TOKEN_JSON is not set");
+            "".to_string()
+        })).unwrap_or_else(|_| {
+            println!("Failed to write token.json");
+        });
     }
 }
